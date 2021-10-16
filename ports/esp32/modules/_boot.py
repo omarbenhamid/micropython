fc10546643b6bc717a8cb059576967c8e84ec813
Omar BENHAMID <contact@obenhamid.me>
2021-10-16 17:53:41 +0200

    Fixing boot.py for LyraT mini: need to pull down Pin 13 to turn SDCard on

--- ports/esp32/modules/_boot.py
+++ ports/esp32/modules/_boot.py
@@ -9,4 +9,13 @@ except OSError:
     import inisetup
     vfs = inisetup.setup()
 
+import machine
+try:
+    #LyraT requires to pull down IO13 for SDCARD to work ...
+    machine.Pin(13, machine.Pin.OUT, machine.Pin.PULL_DOWN)
+    uos.mount(machine.SDCard(), '/sdcard')
+except OSError:
+    print("Failed to mount SDCard")
+    print(OSError)
+
 gc.collect()
